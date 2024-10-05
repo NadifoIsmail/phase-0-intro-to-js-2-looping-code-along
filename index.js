f088d1d631ecd33ca221599aa0f3bb16370015ea
Nadifo Ismail <nadifoismail80@gmail.com>
2024-10-05 11:13:46 +0300

    modifies index.js

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+const names = ["Guadalupe", "Ollie", "Aki"];
+function writeCards() {
+  let messages = [];
+  for (let i = 0; i < names.length; i++) {
+    messages[i] = `Thank you, ${names[i]}, for the wonderful surprise gift!`;
+  }
+  return messages;
+}
+console.log(writeCards());
+
+function countDown() {
+  let number = 10;
+  while (number >= 0) {
+    console.log(number);
+    number--;
+  }
+}
+countDown();
